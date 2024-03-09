8ff6360a7c888ad5af1ca477aeb2d175d8f3543e
ThatOneShortGuy <braxton.brown@outlook.com>
2024-03-09 13:10:58 -0700

    Finish model

--- Model.py
+++ Model.py
@@ -10,11 +10,16 @@ class Model:
 
         fpath = 'stopwords.txt'
         STOPWORDS = read_file_to_list(fpath)
-        word_dict = create_vector_dict(pos_comments + neg_comments, STOPWORDS)
-        self.pos_vec = vectorize_comments(pos_comments, word_dict)
-        self.neg_vec = vectorize_comments(neg_comments, word_dict)
+        self.word_dict = create_vector_dict(pos_comments + neg_comments, STOPWORDS)
+        self.pos_vec = vectorize_comments(pos_comments, self.word_dict)
+        self.neg_vec = vectorize_comments(neg_comments, self.word_dict)
         
         self.comments_sum = np.array(self.pos_vec) + self.neg_vec
+        self.pos_prob_vec = self.pos_vec / self.comments_sum
+        self.neg_prob_vec = 1 - self.pos_prob_vec
     
     def run(self, text: str) -> bool:
-        return True
+        test_vec = vectorize_comment(text, self.word_dict)
+        test_pos = np.dot(test_vec, self.pos_prob_vec)
+        test_neg = np.dot(test_vec, self.neg_prob_vec)
+        return test_pos > test_neg
